4b60b9d973aede2b51684b412d95f4e570fbb4d9
Stefan E-K <stefan.eideloth@gmail.com>
2023-05-15 22:02:56 +0200

    navigation basics done

---
+++ src/config/docs.ts
@@ -0,0 +1,38 @@
+export const docsConfig = {
+  mainNav: [
+    {
+      title: "Documentation",
+      href: "/docs/introduction"
+    },
+    {
+      title: "GitHub",
+      href: "https://github.com/sek-consulting/solid-ui-components",
+      external: true
+    },
+    {
+      title: "Twitter",
+      href: "https://twitter.com/stefan_e_k",
+      external: true
+    }
+  ],
+  sidebarNav: [
+    {
+      title: "Getting Started",
+      items: [
+        {
+          title: "Introduction",
+          href: "/docs/introduction"
+        }
+      ]
+    },
+    {
+      title: "Components",
+      items: [
+        {
+          title: "Button",
+          href: "/docs/components/button"
+        }
+      ]
+    }
+  ]
+}

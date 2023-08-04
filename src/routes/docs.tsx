6142a167a74be5d01b6326ff377aa17af8cf28e5
Stefan E-K <stefan.eideloth@gmail.com>
2023-08-04 14:06:52 +0200

    prelimary code highlighting

--- src/routes/docs.tsx
+++ src/routes/docs.tsx
@@ -1,3 +1,4 @@
+import type { ComponentProps } from "solid-js"
 import { MDXProvider } from "solid-jsx"
 import { Outlet } from "solid-start"
 
@@ -20,7 +21,30 @@ export default function DocsLayout() {
         li: Typography.Li,
         blockquote: Typography.BlockQuote,
         img: Typography.Img,
-        hr: Typography.Hr
+        hr: Typography.Hr,
+        code: (props: ComponentProps<"code">) => {
+          return (
+            <code
+              class="bg-muted relative rounded px-[0.3rem] py-[0.2rem] font-mono text-sm group-[&.code]:bg-zinc-900 group-[&.code]:p-0"
+              {...props}
+            />
+          )
+        },
+        pre: (
+          props: ComponentProps<"pre"> & {
+            "data-meta"?: boolean
+            "data-package"?: boolean
+          }
+        ) => {
+          let preRef: HTMLPreElement | undefined
+          return (
+            <pre
+              ref={preRef}
+              class="code group mb-4 mt-6 max-h-[650px] overflow-x-auto rounded-lg border bg-zinc-900 p-4"
+              {...props}
+            />
+          )
+        }
       }}
     >
       <div class="container flex-1 items-start py-6 md:grid md:grid-cols-[220px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10">
--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,28 @@ import solid from "solid-start/vite"
 import mdx from "@mdx-js/rollup"
 import vercel from "solid-start-vercel"
 
+import rehypePrettyCode from "rehype-pretty-code"
+import { getHighlighter, loadTheme } from "shiki"
+
+import path from "path"
+
 export default defineConfig({
   plugins: [
     {
       ...mdx({
         jsxImportSource: "solid-jsx",
-        providerImportSource: "solid-jsx"
+        providerImportSource: "solid-jsx",
+        rehypePlugins: [
+          [
+            rehypePrettyCode,
+            {
+              getHighlighter: async () => {
+                const theme = await loadTheme(path.join(process.cwd(), "/src/lib/themes/dark.json"))
+                return await getHighlighter({ theme })
+              }
+            }
+          ]
+        ]
       }),
       enforce: "pre"
     },

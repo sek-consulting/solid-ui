ded2cb5c12571b5185ee3d57325a495316fcb485
michaelessiet <emsaa2002@gmail.com>
2023-08-07 00:28:23 +0100

    got build to work

--- packages/core/rollup.config.js
+++ packages/core/rollup.config.js
@@ -1,7 +1,26 @@
 import withSolid from "rollup-preset-solid"
+import alias from "@rollup/plugin-alias"
+import resolve from '@rollup/plugin-node-resolve'
+import path from "path"
+import { fileURLToPath } from "url"
+
+const fileurl = import.meta.url
+const __filepath = fileURLToPath(fileurl)
+const __dirname = path.dirname(__filepath)
 
 export default withSolid({
   input: "./index.tsx",
   targets: ["esm", "cjs"],
-  solidOptions: { hydratable: true, generate: "ssr" }
+  solidOptions: { hydratable: true, generate: "ssr" },
+  plugins: [
+    alias({
+      entries: [
+        {
+          find: "~",
+          replacement: path.resolve(__dirname)
+        }
+      ]
+    }),
+    resolve()
+  ]
 })

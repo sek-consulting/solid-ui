9ebce0a88fd566c04ad35d5c1231d97cd16acb76
Stefan E-K <stefan.eideloth@gmail.com>
2023-10-30 09:45:49 +0100

    fix(docs): fix collapsible demo

--- apps/docs/src/registry/example/collapsible-demo.tsx
+++ apps/docs/src/registry/example/collapsible-demo.tsx
@@ -1,20 +1,19 @@
-import { CollapsibleRoot } from "@kobalte/core/dist/types/collapsible/collapsible-root"
 import { TbChevronDown } from "solid-icons/tb"
 
-import { CollapsibleContent, CollapsibleTrigger } from "../ui/collapsible"
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "~/registry/ui/collapsible"
 
 export default function CollapsibleDemo() {
   return (
-    <CollapsibleRoot>
-      <CollapsibleTrigger>
+    <Collapsible class="w-[300px]">
+      <CollapsibleTrigger class="flex w-full items-center justify-between rounded-md border p-3">
         <span>What is Kobalte ?</span>
         <TbChevronDown class="h-4 w-4" />
       </CollapsibleTrigger>
-      <CollapsibleContent>
+      <CollapsibleContent class="mt-2 rounded-md border p-3">
         Kobalte is a UI toolkit for building accessible web apps and design systems with SolidJS. It
         provides a set of low-level UI components and primitives which can be the foundation for
         your design system implementation.
       </CollapsibleContent>
-    </CollapsibleRoot>
+    </Collapsible>
   )
 }

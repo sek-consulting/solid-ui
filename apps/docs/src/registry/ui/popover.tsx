4eca85eb9e8ecc75351152330af83cedf39fadea
Daugvinas <daugvinasr@gmail.com>
2023-11-13 08:46:16 +0200

    fix: Add missing animations to Dialog and Popover (#39)
    * fix animations for dialog and popover
    
    * remove not used classes

--- apps/docs/src/registry/ui/popover.tsx
+++ apps/docs/src/registry/ui/popover.tsx
@@ -1,4 +1,4 @@
-import type { Component} from "solid-js";
+import type { Component } from "solid-js"
 import { splitProps } from "solid-js"
 
 import { Popover as PopoverPrimitive } from "@kobalte/core"
@@ -17,7 +17,7 @@ const PopoverContent: Component<PopoverPrimitive.PopoverContentProps> = (props) 
     <PopoverPrimitive.Portal>
       <PopoverPrimitive.Content
         class={cn(
-          "bg-popover text-popover-foreground animate-in z-50 w-72 origin-[var(--kb-popover-content-transform-origin)] rounded-md border p-4 shadow-md outline-none",
+          "bg-popover text-popover-foreground data-[expanded]:animate-in data-[closed]:animate-out data-[closed]:fade-out-0 data-[expanded]:fade-in-0 data-[closed]:zoom-out-95 data-[expanded]:zoom-in-95 z-50 w-72 origin-[var(--kb-popover-content-transform-origin)] rounded-md border p-4 shadow-md outline-none",
           props.class
         )}
         {...rest}

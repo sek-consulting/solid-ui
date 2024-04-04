e4071f2b55d69ec19fdd1dae7a6fdf5f4e243852
Stefan E-K <stefan.eideloth@gmail.com>
2024-04-04 21:05:39 +0200

    feat(docs): added instructions for tauri

--- apps/docs/src/components/icons.tsx
+++ apps/docs/src/components/icons.tsx
@@ -51,6 +51,16 @@ export function IconLogo(props: IconProps) {
 
 // ICONS
 
+export function IconAlertTriangle(props: IconProps) {
+  return (
+    <Icon {...props}>
+      <path d="M12 9v4" />
+      <path d="M10.363 3.591l-8.106 13.534a1.914 1.914 0 0 0 1.636 2.871h16.214a1.914 1.914 0 0 0 1.636 -2.87l-8.106 -13.536a1.914 1.914 0 0 0 -3.274 0z" />
+      <path d="M12 16h.01" />
+    </Icon>
+  )
+}
+
 export function IconArrowRight(props: IconProps) {
   return (
     <Icon {...props}>

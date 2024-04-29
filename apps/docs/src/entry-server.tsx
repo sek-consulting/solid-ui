73a7b22d3457dc913cd271a4b33ad3c7a61ef28e
Stefan E-K <stefan.eideloth@gmail.com>
2024-04-29 12:35:40 +0200

    feat: add umami for gdpr compliant analytics

--- apps/docs/src/entry-server.tsx
+++ apps/docs/src/entry-server.tsx
@@ -5,7 +5,14 @@ export default createHandler(() => (
   <StartServer
     document={({ assets, children, scripts }) => (
       <html lang="en">
-        <head>{assets}</head>
+        <head>
+          {assets}
+          <script
+            defer
+            src="https://analytics.eu.umami.is/script.js"
+            data-website-id="ee09d538-8dab-4134-9dca-aad904b65af7"
+          />
+        </head>
         <body>
           <div id="app">{children}</div>
           {scripts}

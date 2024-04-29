8fbbdfa3e86390bd3085363765d7b4a7ffaca78c
Stefan E-K <stefan.eideloth@gmail.com>
2024-04-29 11:03:28 +0200

    fix(docs): resizable demo fixes

--- apps/docs/src/registry/example/resizable-demo.tsx
+++ apps/docs/src/registry/example/resizable-demo.tsx
@@ -3,21 +3,21 @@ import { Resizable, ResizableHandle, ResizablePanel } from "../ui/resizable"
 export default function ResizableDemo() {
   return (
     <Resizable orientation="horizontal" class="max-w-md rounded-lg border">
-      <ResizablePanel initialSize={50}>
+      <ResizablePanel initialSize={0.25} class="overflow-hidden">
         <div class="flex h-[200px] items-center justify-center p-6">
           <span class="font-semibold">One</span>
         </div>
       </ResizablePanel>
       <ResizableHandle withHandle />
-      <ResizablePanel initialSize={50}>
+      <ResizablePanel initialSize={0.75} class="overflow-hidden">
         <Resizable orientation="vertical">
-          <ResizablePanel initialSize={25}>
+          <ResizablePanel initialSize={0.5} class="overflow-hidden">
             <div class="flex h-full items-center justify-center p-6">
               <span class="font-semibold">Two</span>
             </div>
           </ResizablePanel>
           <ResizableHandle />
-          <ResizablePanel initialSize={75}>
+          <ResizablePanel initialSize={0.5} class="overflow-hidden">
             <div class="flex h-full items-center justify-center p-6">
               <span class="font-semibold">Three</span>
             </div>

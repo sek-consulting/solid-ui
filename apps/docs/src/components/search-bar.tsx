e5e94bb28fbf8603e7f47ab6fdbd5c7ecfb1e100
Stefan E-K <stefan.eideloth@gmail.com>
2024-07-01 11:25:09 +0200

    feat: searchbar is now a command dialog component

--- apps/docs/src/components/search-bar.tsx
+++ apps/docs/src/components/search-bar.tsx
@@ -1,129 +1,90 @@
-import { createResource, createSignal, For, onMount, Show } from "solid-js"
-import { isServer } from "solid-js/web"
+import { createEffect, createSignal, For, onCleanup } from "solid-js"
 
-import type { SearchRes } from "~/types/search"
-import ShoSho from "shosho"
+import { useNavigate } from "@solidjs/router"
 
-import { IconCommand, IconHash, IconSearch } from "~/components/icons"
-import { Badge } from "~/registry/ui/badge"
+import { docsConfig } from "~/config/docs"
 import { Button } from "~/registry/ui/button"
 import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTrigger
-} from "~/registry/ui/dialog"
-import { TextField, TextFieldInput } from "~/registry/ui/text-field"
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList
+} from "~/registry/ui/command"
 
-const shortcut = new ShoSho()
-
-async function fetchSearch(value: string) {
-  const url = `https://docs-search-qyz9b.ondigitalocean.app/search?collectionName=solid-ui-docs&q=${value}&queryBy=title,contents`
-  const res = await fetch(url)
-  const data: SearchRes = await res.json()
-  return data
-}
+import { IconCommand, IconFile, IconSearch } from "./icons"
 
 export default function SearchBar() {
-  const [searchValue, setSearchValue] = createSignal("")
-  const [isOpen, setIsOpen] = createSignal(false)
-  const [data] = createResource(searchValue, fetchSearch)
-  data()
+  const [open, setOpen] = createSignal(false)
 
-  onMount(() => {
-    if (!isServer) {
-      shortcut.register("CmdOrCtrl+K", () => {
-        document.getElementById("search-trigger")?.click()
-        return true
-      })
-      shortcut.start()
+  createEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault()
+        setOpen((open) => !open)
+      }
     }
+
+    document.addEventListener("keydown", down)
+
+    onCleanup(() => document.removeEventListener("keydown", down))
   })
 
+  const navigate = useNavigate()
+  const redirect = (url: string) => {
+    setOpen(false)
+    navigate(url)
+  }
+
   return (
-    <Dialog onOpenChange={setIsOpen} open={isOpen()}>
-      <DialogTrigger
-        as={Button<"button">}
+    <>
+      <Button
         id="search-trigger"
         variant="outline"
         class="relative w-full justify-between space-x-4 text-muted-foreground"
+        onClick={() => setOpen(true)}
       >
         <div class="flex items-center space-x-2">
           <IconSearch />
-          <span>Search...</span>
+          <span class="hidden lg:inline-flex">Search documentation...</span>
+          <span class="inline-flex lg:hidden">Search...</span>
         </div>
-
-        <Badge variant="secondary" class="flex items-center ">
-          <IconCommand />
+        <kbd class="pointer-events-none flex select-none items-center gap-1 rounded border bg-muted px-1.5 py-0.5 font-mono text-xs font-medium">
+          <IconCommand stroke-width={1} />
           <span>K</span>
-        </Badge>
-      </DialogTrigger>
-
-      <DialogContent class="flex h-4/5 flex-col">
-        <DialogHeader>
-          <TextField>
-            <TextFieldInput
-              placeholder="Search docs..."
-              value={searchValue()}
-              onKeyUp={(v) => setSearchValue(v.currentTarget.value)}
-              type="text"
-            />
-          </TextField>
-        </DialogHeader>
-
-        <DialogDescription class="overflow-scroll">
-          <Show when={!data.loading} fallback={<p class="text-center">Loading...</p>}>
-            <For each={data()?.hits}>
-              {(item, index) => {
-                const id = `search-content-${index()}`
-                const titleId = `search-title-${index()}`
-
-                setTimeout(() => {
-                  if (document.getElementById(id)) {
-                    document.getElementById(id)!.innerHTML = item.highlight.contents.snippet
-                  }
-                  if (document.getElementById(titleId) && item.highlight.title?.snippet) {
-                    document.getElementById(titleId)!.innerHTML = item.highlight.title.snippet
-                  }
-                }, 1)
-
-                return (
-                  <a
-                    href={item.document.uri}
-                    onClick={() => setIsOpen(false)}
-                    class="duration-400 animate-in fade-in-0"
-                  >
-                    <div class="grid grid-cols-[25px_1fr] items-start rounded p-4 hover:bg-black/30 dark:hover:bg-white/30">
-                      <IconHash class="size-5" />
-                      <div class="space-y-1">
-                        <p
-                          class="text-base font-medium leading-none text-black dark:text-white"
-                          id={titleId}
-                        >
-                          {item.document.title}
-                        </p>
-                        <p class="text-sm text-muted-foreground" id={id} />
-                        Th
-                      </div>
-                    </div>
-                  </a>
-                )
-              }}
+        </kbd>
+      </Button>
+      <CommandDialog open={open()} onOpenChange={setOpen}>
+        <CommandInput placeholder="Search documentation..." />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup heading="Links">
+            <For each={docsConfig.mainNav.filter((item) => !item.external)}>
+              {(item) => (
+                <CommandItem onSelect={() => redirect(item.href)}>
+                  <IconFile class="mr-2" />
+                  {item.title}
+                </CommandItem>
+              )}
             </For>
-          </Show>
-        </DialogDescription>
-
-        <DialogFooter>
-          <p class="opacity-50">
-            Search by{" "}
-            <a href="https://x.com/devshogun" target="_blank" class="underline">
-              Michael Essiet
-            </a>
-          </p>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
+          </CommandGroup>
+          <For each={docsConfig.sidebarNav}>
+            {(category) => (
+              <CommandGroup heading={category.title}>
+                <For each={category.items}>
+                  {(item) => (
+                    <CommandItem onSelect={() => redirect(item.href)}>
+                      <IconFile class="mr-2" />
+                      {item.title}
+                    </CommandItem>
+                  )}
+                </For>
+              </CommandGroup>
+            )}
+          </For>
+        </CommandList>
+      </CommandDialog>
+    </>
   )
 }

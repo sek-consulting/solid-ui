321fbb81a0b9c69be27297b85e86f73065fd82a9
Stefan E-K <stefan.eideloth@gmail.com>
2024-07-24 14:28:55 +0200

    removed all cli stuff for clean restart

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,8 +1,7 @@
 #! /usr/bin/env node
-import { add } from "~/commands/add"
-import init from "~/commands/init"
-import { getPackageInfo } from "~/utils/get-package-info"
 import { Command } from "commander"
+
+import { getPackageInfo } from "~/utils/get-package-info"
 
 process.on("SIGINT", () => process.exit(0))
 process.on("SIGTERM", () => process.exit(0))
@@ -12,23 +11,11 @@ async function main() {
 
   const packageInfo = getPackageInfo()
 
-  const program = new Command()
+  new Command()
     .name("solidui-cli")
-    .description("Add SolidUI components to your project")
-    .version(packageInfo.version || "0.0.0", "-v, --version")
-
-  program
-    .command("init")
-    .description("Initialize and install the necessary things to make use of SolidUI")
-    .action(async () => await init())
-
-  program
-    .command("add")
-    .argument("[components...]", "Components to be added to your project")
-    .description("Add a component to your project")
-    .action(async (component: string[]) => await add(component))
-
-  program.parse()
+    .description("add SolidUI components to your project")
+    .version(packageInfo.version || "0.0.0", "-v, --version", "display the version number")
+    .parse()
 }
 
 main()

9cac3d472ba9ca39342bb8732e5e134b02a04b94
Daniel Macák <34311965+daelmaak@users.noreply.github.com>
2024-10-07 07:54:36 +0200

    add: indeterminate state for checkbox (#135)
    * feat: indeterminate state for checkbox
    
    * Create fuzzy-pears-watch.md
    
    ---------
    
    Co-authored-by: Stefan E-K <stefan.eideloth@gmail.com>

---
+++ .changeset/fuzzy-pears-watch.md
@@ -0,0 +1,5 @@
+---
+"docs": patch
+---
+
+add: indeterminate state for checkbox
--- apps/docs/src/registry/ui/checkbox.tsx
+++ apps/docs/src/registry/ui/checkbox.tsx
@@ -1,5 +1,5 @@
 import type { ValidComponent } from "solid-js"
-import { splitProps } from "solid-js"
+import { Match, splitProps, Switch } from "solid-js"
 
 import * as CheckboxPrimitive from "@kobalte/core/checkbox"
 import type { PolymorphicProps } from "@kobalte/core/polymorphic"
@@ -16,20 +16,38 @@ const Checkbox = <T extends ValidComponent = "div">(
   return (
     <CheckboxPrimitive.Root class={cn("items-top group flex space-x-2", local.class)} {...others}>
       <CheckboxPrimitive.Input class="peer" />
-      <CheckboxPrimitive.Control class="size-4 shrink-0 rounded-sm border border-primary ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 peer-focus-visible:outline-none peer-focus-visible:ring-2 peer-focus-visible:ring-ring peer-focus-visible:ring-offset-2 data-[checked]:border-none data-[checked]:bg-primary data-[checked]:text-primary-foreground">
+      <CheckboxPrimitive.Control class="size-4 shrink-0 rounded-sm border border-primary ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 peer-focus-visible:outline-none peer-focus-visible:ring-2 peer-focus-visible:ring-ring peer-focus-visible:ring-offset-2 data-[checked]:border-none data-[indeterminate]:border-none data-[checked]:bg-primary data-[indeterminate]:bg-primary data-[checked]:text-primary-foreground data-[indeterminate]:text-primary-foreground">
         <CheckboxPrimitive.Indicator>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="size-4"
-          >
-            <path d="M5 12l5 5l10 -10" />
-          </svg>
+          <Switch>
+            <Match when={!others.indeterminate}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="size-4"
+              >
+                <path d="M5 12l5 5l10 -10" />
+              </svg>
+            </Match>
+            <Match when={others.indeterminate}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="size-4"
+              >
+                <path d="M5 12l14 0" />
+              </svg>
+            </Match>
+          </Switch>
         </CheckboxPrimitive.Indicator>
       </CheckboxPrimitive.Control>
     </CheckboxPrimitive.Root>
